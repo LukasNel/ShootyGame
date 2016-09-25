dbb6002be413e7ac863ca39f02b277601d5efd44
LukasNel <lukasabrie@gmail.com>
2016-09-25 22:50:52 +0200

    Created project and Implemented Dungeon Generation
    Used drunken walk algorithm to mostly implement dungeon generation

--- ShootyGame/Assets/terrainGenerator.cs
+++ ShootyGame/Assets/terrainGenerator.cs
@@ -12,36 +12,74 @@ public class terrainGenerator : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
-        gridSize = (int)(Mathf.Max(this.transform.localScale.x, this.transform.localScale.z)/blockSize);
+        gridSize = (int)(this.transform.localScale.x/blockSize);
         grid = DungeonGenerationDrunkenWalk(gridSize, iterations, minArea, false, true);
 
         int tI = 0;
         GameObject curCube;
-        Vector3 startPos = this.transform.position - new Vector3(this.transform.localScale.x/2 - blockSize/2,0, this.transform.localScale.z/2 - blockSize / 2);
+        Vector3 startPos = this.transform.position - new Vector3(this.transform.localScale.x / 2 - blockSize / 2, 0, this.transform.localScale.z / 2 - blockSize / 2);
         NavMeshObstacle navMeshObstacle;
         Debug.Log(gridSize);
+
+        PrintDungeon(grid, gridSize);
+        int count = 0;
+        startPos = this.transform.position - 0.5f *
+            (new Vector3(this.transform.localScale.x - blockSize, 0, this.transform.localScale.z - blockSize));
+        for (int i = 0; i < gridSize; i++) {
+            grid[i, gridSize - 1] = false;
+            grid[gridSize - 1,i]  = false;
+            grid[i, 0]            = false;
+            grid[0, i]            = false;
+        }
+        
         //The order of variables is correct, DO NOT CHANGE, otherwise this code "i += tI" won;t work.
         for (int j = 0; j < gridSize; j++) { 
-            for (int i = 0; i < gridSize;i++)
+            for (int i = 0;i < gridSize;i++)
             {
-                if (!grid[i,j]) {
+                if (grid[i,j] == false) {
+                    count++;
                     tI = 0;
-                    while (i + tI < gridSize && grid[i + tI, j] == false)tI++;
+                   while (i + tI < gridSize && grid[i + tI, j] == false)
+                    {
+
+                      //    grid[i + tI, j] = true;
+
+                        tI++;
+                    }
                     tI--;
                     curCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                    curCube.transform.position = startPos + new Vector3((float)i+(((float)tI)/2)*blockSize,0, j*blockSize);
-                    curCube.transform.localScale = new Vector3((((float)tI) / 2) * blockSize,blockHeight,blockSize);
-
+                    curCube.transform.position = startPos + new Vector3((i+(((float)tI)/2))*blockSize,0, j*blockSize);
+                    curCube.transform.localScale = new Vector3(tI * blockSize + blockSize,blockHeight,blockSize);
+                    /*
                     navMeshObstacle = curCube.AddComponent<NavMeshObstacle>();
                     navMeshObstacle.carving = true;
                     navMeshObstacle.carveOnlyStationary = true;
                     navMeshObstacle.shape = NavMeshObstacleShape.Box;
-
-                    i += tI;            
+                    */
+                    
+                   i += tI;
+                    Debug.Log(i);
                 }
             }
-            
+            //Debug.Log(i + " + " + tI);
+
         }
+        PrintDungeon(grid, gridSize);
+        Debug.Log(count);
+        Debug.Log(gridSize * gridSize);
+        Debug.Log(minArea);
+    }
+    void PrintDungeon(bool[,] dungeon, int fgridsize) {
+        string tstr = "";
+        for (int i = 0; i < fgridsize; i++)
+        {
+            for (int j = 0; j < fgridsize; j++)
+            {
+                tstr += (dungeon[i, j]) ? "1" : " ";
+            }
+            tstr += "\n";
+        }
+        Debug.Log(tstr);
     }
     bool[,] DungeonGenerationDrunkenWalk(int fgridSize,int fiterations,int fminArea,bool wrap,bool noDiagonal) {
         Debug.Log(fgridSize);
@@ -50,7 +88,8 @@ public class terrainGenerator : MonoBehaviour {
         int curArea = 0;
         int fx = fgridSize / 2;
         int fy = fgridSize / 2;
-
+        //fx = Random.Range(0, fgridSize);
+        //fy = Random.Range(0, fgridSize);
         for (int i = 0; i < fgridSize; i++) for (int j = 0; j < fgridSize; j++) fgrid[i, j] = false;
 
         for (int i = 0; i < fiterations || curArea < fminArea; i++)
@@ -63,10 +102,12 @@ public class terrainGenerator : MonoBehaviour {
             {
                 if (Random.Range(0, 2) == 1)
                 {
-                    fx += Random.Range(-1, 2);
+                    fx += (Random.Range(0, 2) == 1) ? -1 : 1; //Random.Range(-1, 2);
+                    //fx += Random.Range(-1, 2);
                 }
                 else {
-                    fy += Random.Range(-1, 2);
+                    //fy += Random.Range(-1, 2);
+                    fy += (Random.Range(0, 2) == 1) ? -1 : 1;
                 }
             }
             else {
@@ -89,7 +130,7 @@ public class terrainGenerator : MonoBehaviour {
                 fy = Mathf.Max(fy, 0);
             }
         }
-
+        minArea = curArea;
         return fgrid;
 
     }
